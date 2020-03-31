a3dfc6bdbd993dabdf7c07d8ff9d2664db5108b7
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 15:38:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,10 +41,11 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input.to_i - 1
+  index = input_to_index(input)
 
     if valid_move?(board,index)
       move(board, index, "X")
+      display_board(board)
     elsif valid_move?(board,index) == false
       puts "Please enter valid number."
       turn(board)
